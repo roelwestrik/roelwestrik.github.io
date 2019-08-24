215fc1bad24421f337f0b0ed8ea76e40f4269d8d
roelwestrik <52472292+roelwestrik@users.noreply.github.com>
2019-08-25 01:42:09 +0200

    mobile page update

--- pages/mobile/js/mobile.js
+++ pages/mobile/js/mobile.js
@@ -1,23 +1,42 @@
+var btnSize = 120;
+var btnY = 260;
+
 function setup(){
     createCanvas(windowWidth-10, windowHeight-20);
     colorMode(RGB,255,255,255,255);
     textAlign(CENTER, CENTER);
     
-    fill(255);
-    noStroke();
 }
 
 function draw(){
     translate(windowWidth/2, windowHeight/2);
-    
     background(0);
+    
+    fill(255);
+    noStroke();
 
     textSize(34);
-    text('The Mobile version of this website is still under construction',0,0);
+    text('The Mobile version of this website is still under construction',0,-100);
     textSize(28);
-    text('Please visit the desktop version for full functionality',0,40);
+    text('Please visit the desktop version for full functionality',0,-40);
+    text('If you are looking for my portfolio,',0,60);
+    text('you can click here to download it directly',0,100);
+
+    noFill();
+    stroke(255);
+    strokeWeight(2);
+
+    ellipse(0,btnY,btnSize);
 }
 
 function windowResized() {
     resizeCanvas(windowWidth-10, windowHeight-20);
+}
+
+function touchStarted(){
+    print(dist(mouseX-windowWidth/2, mouseY-windowHeight/2, 0, btnY));
+    if (dist(mouseX-windowWidth/2, mouseY-windowHeight/2, 0, btnY)<btnSize){
+        window.open("imgs/portfolio_web_082019.pdf");
+        print('YES');
+    }
 }
